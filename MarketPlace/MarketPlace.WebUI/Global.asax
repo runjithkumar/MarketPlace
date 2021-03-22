0b8ff381b8d7e9b936d91e1568ef83b0266bdac8
Ranjith Kumar <rkvanc2121@gmail.com>
2021-03-22 11:32:33 -0700

    Initial Solution Setup to MarketPlace

---
+++ MarketPlace/MarketPlace.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MarketPlace.WebUI.MvcApplication" Language="C#" %>
---
+++ MarketPlace/MarketPlace.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MarketPlace.WebUI.Startup))]
+namespace MarketPlace.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
